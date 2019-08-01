9830054003d7dfac8aeeb19a78e92637ed013da9
mhogar <mhogar98@gmail.com>
2019-07-31 20:42:52 -0400

    Converted cpp code to c# code

---
+++ Blueprint.Logic/ObjDefs.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blueprint.Logic
+{
+    public enum AccessModifier
+    {
+        PUBLIC,
+        PROTECTED,
+        PRIVATE
+    }
+
+    public class VariableObj
+    {
+        public string Type
+        {
+            get;
+            set;
+        }
+
+        public string Name
+        {
+            get;
+            set;
+        }
+
+        public VariableObj(VariableObj variableObj)
+            : this(variableObj.Type, variableObj.Name)
+        {
+        }
+
+        public VariableObj(string type = "", string name = "")
+        {
+            Type = type;
+            Name = name;
+        }
+    }
+
+    public class MemberObj
+    {
+        public AccessModifier Access
+        {
+            get;
+            set;
+        }
+
+        public VariableObj Variable
+        {
+            get;
+            set;
+        }
+
+        public MemberObj(AccessModifier access, VariableObj variable)
+        {
+            Access = access;
+            Variable = variable;
+        }
+
+        public MemberObj(AccessModifier access = AccessModifier.PUBLIC, string type = "", string name = "")
+            : this(access, new VariableObj(type, name))
+        {
+        }
+    }
+
+    public class FunctionObj
+    {
+        public AccessModifier Access
+        {
+            get;
+            set;
+        }
+
+        public VariableObj TypeAndName
+        {
+            get;
+            set;
+        }
+
+        public List<VariableObj> FuncParams
+        {
+            get;
+        }
+
+        public FunctionObj(AccessModifier access, VariableObj typeAndName)
+        {
+            Access = access;
+            TypeAndName = typeAndName;
+            FuncParams = new List<VariableObj>();
+        }
+
+        public FunctionObj(AccessModifier access = AccessModifier.PUBLIC, string type = "", string name = "")
+            : this(access, new VariableObj(type, name))
+        {
+        }
+    }
+}
---
+++ Blueprint/Program.cs
@@ -0,0 +1,26 @@
+﻿using Blueprint.Logic;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Blueprint
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var writer = new CppWriter("Foo");
+
+            //add members, properties, and functions
+            writer.AddMember(new MemberObj(AccessModifier.PROTECTED, "int", "foo1"));
+            writer.AddProperty(new MemberObj(AccessModifier.PUBLIC, "bool", "foo2"));
+
+            const string OUT_DIR = "C:/Users/mhoga/Documents/Projects/Blueprint/Bin/";
+            writer.Write(OUT_DIR);
+
+            Console.ReadLine();
+        }
+    }
+}
