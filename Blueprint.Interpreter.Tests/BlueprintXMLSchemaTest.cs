b7e6e27675b5382ae27b7b9461bccb831483b7fc
mhogar <mhogar98@gmail.com>
2019-08-17 13:55:40 -0400

    Began writing unit tests for blueprint xml schema validations

---
+++ Blueprint.Interpreter.Tests/BlueprintXMLSchemaTest.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Diagnostics;
+using System.Xml;
+using System.Xml.Schema;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Blueprint.Interpreter.Tests
+{
+    [TestClass]
+    public class BlueprintXMLSchemaTest
+    {
+        private XmlDocument _doc;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            _doc = new XmlDocument();
+            _doc.Load(
+                "C:/Users/mhoga/Documents/Projects/Blueprint/Blueprint.Interpreter.Tests/testBlueprint.xml");
+            _doc.Schemas.Add(null,
+                "C:/Users/mhoga/Documents/Projects/Blueprint/Blueprint.Interpreter/BlueprintXMLSchema.xsd");
+        }
+
+        public bool ValidateXml()
+        {
+            _doc.Validate(new ValidationEventHandler((object sender, ValidationEventArgs args) =>
+            {
+                if (args.Severity == XmlSeverityType.Error)
+                {
+                    throw args.Exception;
+                }
+            }));
+
+            return true;
+        }
+
+        [TestMethod, TestCategory("General")]
+        public void TestBlueprintFileValid()
+        {
+            Assert.IsTrue(ValidateXml());
+        }
+
+        [TestMethod, TestCategory("General")]
+        public void TestRootNotBlueprintInvalid()
+        {
+            //Remove blueprint node and add invalid node
+            _doc.RemoveAll();
+            _doc.AppendChild(_doc.CreateElement("NotBlueprint"));
+
+            Assert.ThrowsException<XmlSchemaValidationException>(() => ValidateXml());
+        }
+
+        [TestMethod, TestCategory("Blueprint")]
+        public void TestBlueprintNoFileInvalid()
+        {
+            //remove file node
+            XmlNode node = _doc.LastChild;
+            node.RemoveChild(node.SelectSingleNode("File"));
+
+            Assert.ThrowsException<XmlSchemaValidationException>(() => ValidateXml());
+        }
+
+        [TestMethod, TestCategory("Blueprint")]
+        public void TestBlueprintInvalidChild()
+        {
+            //remove file node
+            XmlNode node = _doc.LastChild;
+            node.AppendChild(_doc.CreateElement("NotFile"));
+
+            Assert.ThrowsException<XmlSchemaValidationException>(() => ValidateXml());
+        }
+
+        [TestMethod, TestCategory("File")]
+        public void TestFileNoNameInvalid()
+        {
+            //remove file's name
+            XmlNode node = _doc.LastChild.FirstChild;
+            node.Attributes.Remove(node.Attributes["name"]);
+
+            Assert.ThrowsException<XmlSchemaValidationException>(() => ValidateXml());
+        }
+    }
+}
---
+++ Blueprint.Interpreter.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Blueprint.Interpreter.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("Blueprint.Interpreter.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6e078969-1a47-47fa-90bb-cacaa2165d1b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
