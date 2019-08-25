bbd6223255113537efc1ba6d396d222a60985d44
mhogar <mhogar98@gmail.com>
2019-08-25 19:31:45 -0400

    Updated blueprint spec and finished tests

--- Blueprint.Interpreter.Tests/BlueprintXMLSchemaTest.cs
+++ Blueprint.Interpreter.Tests/BlueprintXMLSchemaTest.cs
@@ -12,12 +12,13 @@ namespace Blueprint.Interpreter.Tests
         private XmlDocument _doc;
         private XmlNode _blueprintNode;
         private XmlNode _fileNode;
-        private XmlNode _fileContentNode;
         private XmlNode _fileVariableNode;
         private XmlNode _fileFunctionNode;
         private XmlNode _fileClassNode;
-        private XmlNode _fileClassAccessModifierNode;
-        private XmlNode _fileClassContentNode;
+        private XmlNode _fileClassContructorNode;
+        private XmlNode _fileClassVariableNode;
+        private XmlNode _fileClassFunctionNode;
+        private XmlNode _fileClassPropertyNode;
 
         [TestInitialize]
         public void Initialize()
@@ -28,16 +29,23 @@ namespace Blueprint.Interpreter.Tests
             _doc.Schemas.Add(null,
                 "C:/Users/mhoga/Documents/Projects/Blueprint/Blueprint.Interpreter/BlueprintXMLSchema.xsd");
 
+            //init node refs
             _blueprintNode = _doc.LastChild;
-            _fileNode = _blueprintNode.FirstChild;
-            _fileContentNode = _fileNode.FirstChild;
-            _fileVariableNode = _fileContentNode.FirstChild;
-            _fileFunctionNode = _fileContentNode.ChildNodes[1];
-            _fileClassNode = _fileContentNode.ChildNodes[2];
-            _fileClassAccessModifierNode = _fileClassNode.FirstChild;
-            _fileClassContentNode = _fileClassNode.LastChild;
+            _fileNode = _blueprintNode.FirstChild.FirstChild; //Blueprint/Files/File
+
+            XmlNode fileContentNode = _fileNode.FirstChild; //File/content
+            _fileVariableNode = fileContentNode.ChildNodes[0].FirstChild; //content/Variables/Variable
+            _fileFunctionNode = fileContentNode.ChildNodes[1].FirstChild; //content/Functions/Function
+            _fileClassNode = fileContentNode.ChildNodes[2].FirstChild; //content/Classes/Class
+
+            XmlNode fileClassContentNode = _fileClassNode.ChildNodes[1]; //Class/content
+            _fileClassContructorNode = fileClassContentNode.ChildNodes[0]; //content/Constructor
+            _fileClassVariableNode = fileClassContentNode.ChildNodes[1].FirstChild; //content/Variables/Variable
+            _fileClassFunctionNode = fileClassContentNode.ChildNodes[2].FirstChild; //content/Functions/Function
+            _fileClassPropertyNode = fileClassContentNode.ChildNodes[3].FirstChild; //content/Properties/Property
         }
 
+        #region Helpers
         private bool ValidateXml()
         {
             _doc.Validate(new ValidationEventHandler((object sender, ValidationEventArgs args) =>
@@ -65,61 +73,84 @@ namespace Blueprint.Interpreter.Tests
             Assert.IsTrue(ValidateXml());
         }
 
-        private void TestNodeMinOccurences(XmlNode parentNode, XmlNode childNode, uint minOccurences)
+        private void TestNodeOccurences(XmlNode node, uint minOccurences, uint maxOccurences)
         {
-            if (minOccurences == 0) return;
+            XmlNode parentNode = node.ParentNode;
 
             //remove all occurences of child
-            foreach (XmlNode node in parentNode.SelectNodes(childNode.Name))
+            foreach (XmlNode childNode in parentNode.SelectNodes(node.Name))
             {
-                parentNode.RemoveChild(node);
+                parentNode.RemoveChild(childNode);
             }
 
-            //test one less than min is invalid
-            for (uint i = 0; i < minOccurences - 1; i++)
+            if (minOccurences == 0)
             {
-                parentNode.AppendChild(childNode.CloneNode(true));
+                //test if still valid with no occurences
+                Assert.IsTrue(ValidateXml());
             }
-            Assert.ThrowsException<XmlSchemaValidationException>(() => ValidateXml());
+            else
+            {
+                //test one less than min is invalid
+                for (uint i = 0; i < minOccurences - 1; i++)
+                {
+                    parentNode.AppendChild(node.CloneNode(true));
+                }
+                Assert.ThrowsException<XmlSchemaValidationException>(() => ValidateXml());
 
-            //test exactly min is valid
-            parentNode.AppendChild(childNode.CloneNode(true));
-            Assert.IsTrue(ValidateXml());
-        }
+                //test exactly min is valid
+                parentNode.AppendChild(node.CloneNode(true));
+                Assert.IsTrue(ValidateXml());
+            }
 
-        private void TestNodeMaxOccurences(XmlNode parentNode, XmlNode childNode, uint maxOccurences)
-        {
             //NOTE: maxOccurences = 0 means unbounded
-            if (maxOccurences == 0) return;
-
-            //remove all occurences of child
-            foreach (XmlNode node in parentNode.SelectNodes(childNode.Name))
+            if (maxOccurences == 0)
             {
-                parentNode.RemoveChild(node);
+                //hard to test unbounded, so just test if more than one occurence (the default) is valid
+                for (uint i = minOccurences; i < 2; i++)
+                {
+                    parentNode.AppendChild(node.CloneNode(true));
+                }
+                Assert.IsTrue(ValidateXml());
             }
-
-            //test exactly max is valid
-            for (uint i = 0; i < maxOccurences; i++)
+            else
             {
-                parentNode.AppendChild(childNode.CloneNode(true));
-            }
-            Assert.IsTrue(ValidateXml());
+                //test exactly max is valid
+                for (uint i = minOccurences; i < maxOccurences; i++)
+                {
+                    parentNode.AppendChild(node.CloneNode(true));
+                }
+                Assert.IsTrue(ValidateXml());
 
-            //test one more than max is invalid
-            parentNode.AppendChild(childNode.CloneNode(true));
-            Assert.ThrowsException<XmlSchemaValidationException>(() => ValidateXml());
+                //test one more than max is invalid
+                parentNode.AppendChild(node.CloneNode(true));
+                Assert.ThrowsException<XmlSchemaValidationException>(() => ValidateXml());
+            }
         }
+        #endregion
 
+        #region General
         [TestMethod, TestCategory("General")]
         public void TestBlueprintFileValid()
         {
             Assert.IsTrue(ValidateXml());
         }
+        #endregion
 
-        [TestMethod, TestCategory("Blueprint")]
-        public void TestBlueprintFileMinOccurences()
+        #region Blueprint
+
+        #endregion
+
+        #region File
+        [TestMethod, TestCategory("File")]
+        public void TestBlueprintFilesOccurences()
         {
-            TestNodeMinOccurences(_blueprintNode, _fileNode, 1);
+            TestNodeOccurences(_fileNode.ParentNode, 1, 1);
+        }
+
+        [TestMethod, TestCategory("File")]
+        public void TestFilesFileOccurences()
+        {
+            TestNodeOccurences(_fileNode, 1, 0);
         }
 
         [TestMethod, TestCategory("File")]
@@ -129,9 +160,16 @@ namespace Blueprint.Interpreter.Tests
         }
 
         [TestMethod, TestCategory("File")]
-        public void TestFileContentMaxOccurences()
+        public void TestFileContentOccurences()
         {
-            TestNodeMaxOccurences(_fileNode, _fileContentNode, 1);
+            TestNodeOccurences(_fileVariableNode.ParentNode, 0, 1);
+        }
+
+        #region FileVariable
+        [TestMethod, TestCategory("File"), TestCategory("FileVariable")]
+        public void TestFileVariablesOccurences()
+        {
+            TestNodeOccurences(_fileVariableNode.ParentNode, 0, 1);
         }
 
         [TestMethod, TestCategory("File"), TestCategory("FileVariable")]
@@ -145,7 +183,21 @@ namespace Blueprint.Interpreter.Tests
         {
             TestAttributeRequired(_fileVariableNode, "type");
         }
-        
+
+        [TestMethod, TestCategory("File"), TestCategory("FileVariable")]
+        public void TestFileVariableOccurences()
+        {
+            TestNodeOccurences(_fileVariableNode, 1, 0);
+        }
+        #endregion
+
+        #region FileFunction
+        [TestMethod, TestCategory("File"), TestCategory("FileFunction")]
+        public void TestFileFunctionsOccurences()
+        {
+            TestNodeOccurences(_fileFunctionNode.ParentNode, 0, 1);
+        }
+
         [TestMethod, TestCategory("File"), TestCategory("FileFunction")]
         public void TestFileFunctionNameRequired()
         {
@@ -164,6 +216,20 @@ namespace Blueprint.Interpreter.Tests
             TestAttributeOptional(_fileFunctionNode, "params");
         }
 
+        [TestMethod, TestCategory("File"), TestCategory("FileFunction")]
+        public void TestFileFunctionOccurences()
+        {
+            TestNodeOccurences(_fileFunctionNode, 1, 0);
+        }
+        #endregion
+
+        #region FileClass
+        [TestMethod, TestCategory("File")]
+        public void TestFileClassesOccurences()
+        {
+            TestNodeOccurences(_fileClassNode.ParentNode, 0, 1);
+        }
+
         [TestMethod, TestCategory("File"), TestCategory("FileClass")]
         public void TestFileClassNameRequired()
         {
@@ -171,16 +237,147 @@ namespace Blueprint.Interpreter.Tests
         }
 
         [TestMethod, TestCategory("File"), TestCategory("FileClass")]
-        public void TestFileClassAccessModifierMaxOccurences()
+        public void TestFileClassOccurences()
         {
-            TestNodeMaxOccurences(_fileClassNode, _fileClassAccessModifierNode, 1);
+            TestNodeOccurences(_fileClassNode, 1, 0);
         }
-        
+
         [TestMethod, TestCategory("File"), TestCategory("FileClass")]
-        public void TestFileClassContentMaxOccurences()
+        public void TestFileClassAccessModifierOccurences()
         {
-            TestNodeMaxOccurences(_fileClassNode, _fileClassContentNode, 1);
+            TestNodeOccurences(_fileClassNode.FirstChild, 0, 1);
         }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass")]
+        public void TestFileClassContentOccurences()
+        {
+            TestNodeOccurences(_fileClassVariableNode.ParentNode, 0, 1);
+        }
+
+        #region FileClassContructor
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassContructor")]
+        public void TestFileClassContructorParamsOptional()
+        {
+            TestAttributeOptional(_fileClassContructorNode, "params");
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassContructor")]
+        public void TestFileClassContructorOccurences()
+        {
+            TestNodeOccurences(_fileClassContructorNode, 0, 1);
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassContructor")]
+        public void TestFileClassConstructorAccessModifierOccurences()
+        {
+            TestNodeOccurences(_fileClassContructorNode.FirstChild, 0, 1);
+        }
+        #endregion
+
+        #region FileClassVariable
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassVariable")]
+        public void TestFileClassVariablesOccurences()
+        {
+            TestNodeOccurences(_fileClassVariableNode.ParentNode, 0, 1);
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassVariable")]
+        public void TestFileClassVariableNameRequired()
+        {
+            TestAttributeRequired(_fileClassVariableNode, "name");
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassVariable")]
+        public void TestFileClassVariableTypeRequired()
+        {
+            TestAttributeRequired(_fileClassVariableNode, "type");
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassVariable")]
+        public void TestFileClassVariableOccurences()
+        {
+            TestNodeOccurences(_fileClassVariableNode, 1, 0);
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassVariable")]
+        public void TestFileClassVariableAccessModifierOccurences()
+        {
+            TestNodeOccurences(_fileClassVariableNode.FirstChild, 0, 1);
+        }
+        #endregion
+
+        #region FileClassFunction
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassFunction")]
+        public void TestFileClassFunctionsOccurences()
+        {
+            TestNodeOccurences(_fileClassFunctionNode.ParentNode, 0, 1);
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClassFunction")]
+        public void TestFileClassFunctionNameRequired()
+        {
+            TestAttributeRequired(_fileClassFunctionNode, "name");
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClassFunction")]
+        public void TestFileClassFunctionReturnTypeRequired()
+        {
+            TestAttributeRequired(_fileClassFunctionNode, "returnType");
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClassFunction")]
+        public void TestFileClassFunctionParamsOptional()
+        {
+            TestAttributeOptional(_fileClassFunctionNode, "params");
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassFunction")]
+        public void TestFileClassFunctionOccurences()
+        {
+            TestNodeOccurences(_fileClassFunctionNode, 1, 0);
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassFunction")]
+        public void TestFileClassFunctionAccessModifierOccurences()
+        {
+            TestNodeOccurences(_fileClassFunctionNode.FirstChild, 0, 1);
+        }
+        #endregion
+
+        #region FileClassProperty
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassProperty")]
+        public void TestFileClassPropertiesOccurences()
+        {
+            TestNodeOccurences(_fileClassPropertyNode.ParentNode, 0, 1);
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassProperty")]
+        public void TestFileClassPropertyNameRequired()
+        {
+            TestAttributeRequired(_fileClassPropertyNode, "name");
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassProperty")]
+        public void TestFileClassPropertyTypeRequired()
+        {
+            TestAttributeRequired(_fileClassPropertyNode, "type");
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassProperty")]
+        public void TestFileClassPropertyOccurences()
+        {
+            TestNodeOccurences(_fileClassPropertyNode, 1, 0);
+        }
+
+        [TestMethod, TestCategory("File"), TestCategory("FileClass"), TestCategory("FileClassProperty")]
+        public void TestFileClassPropertyAccessModifierOccurences()
+        {
+            TestNodeOccurences(_fileClassPropertyNode.FirstChild, 0, 1);
+        }
+        #endregion
+
+        #endregion
+        #endregion
 
     }
 }
